a54d845a8b327d940253a64276e90d9bcba45249
carmeljane <carmelbarwick@gmail.com>
2024-03-29 18:47:02 +0000

    app and router set up

--- server.js
+++ server.js
@@ -1,5 +1,11 @@
 const express = require('express');
 const app = express();
-const port = 3000;
+const apiRouter = require("./routers/api.router");
+const cors = require('cors');
 
-// Define your API routes here
+app.use(cors());
+app.use(express.json());
+
+app.use("/api", apiRouter);
+
+module.exports = app;
