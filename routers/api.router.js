be48cc05143acdaee818c7c2f8f89236d489c010
carmeljane <carmelbarwick@gmail.com>
2024-04-07 11:45:33 +0100

    users router

--- routers/api.router.js
+++ routers/api.router.js
@@ -1,6 +1,9 @@
 const apiRouter= require("express").Router();
 const getAllEndpoints = require("../controllers/api.controller");
+const usersRouter = require("./users.router");
 
 apiRouter.get("/", getAllEndpoints);
+
+apiRouter.use("/users", usersRouter);
 
 module.exports = apiRouter;
---
+++ routers/users.router.js
@@ -0,0 +1,6 @@
+const { getAllUsers } = require("../controllers/users.controller");
+
+const usersRouter = require('express').Router()
+usersRouter.route("/").get(getAllUsers);
+
+module.exports = usersRouter;
